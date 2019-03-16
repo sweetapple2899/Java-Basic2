1fb6a96b9509b48dcb05c97925e86cd5ac74d0f4
muhlisadiwiguna <muhlisadiwiguna@thames>
2019-03-16 18:05:16 +0700

    adding package while loop into control flow statements

---
+++ control flow statements/src/whileloop/WhileLoop.java
@@ -0,0 +1,15 @@
+package whileloop;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+
+		int x = 1;
+
+		while (x <= 10) {
+			System.out.println("Hello " + x);
+			x++;
+		}
+	}
+
+}
