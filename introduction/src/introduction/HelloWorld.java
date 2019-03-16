22bccd0c88956acae101457c8eb8193d4f7650ef
muhlisadiwiguna <muhlisadiwiguna@thames>
2019-03-16 18:00:01 +0700

    adding project introduction and control flow statements

---
+++ introduction/src/introduction/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * This java program will print out 'Hello World!'.
+ * The text after '//' sign is called comment that will not be executed.
+ */
+
+// The package name called 'introduction'
+package introduction; 
+
+//The class name is called 'HelloWorld'
+public class HelloWorld {  
+
+	public static void main(String[] args) { // This line is the start of main method. 
+		
+		System.out.println("Hello World!"); // This is the line that print out 'Hello World'.
+		
+	} // This line is the start of main method.
+
+}
